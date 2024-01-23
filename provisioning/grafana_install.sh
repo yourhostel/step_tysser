fc0852fbd9601e0a264e7e23058318233d635565
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-01-23 05:56:56 +0200

    correction grafana_install.sh

--- provisioning/grafana_install.sh
+++ provisioning/grafana_install.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-Оновлення списку пакетів та встановлення Grafana
+# Оновлення списку пакетів та встановлення Grafana
 sudo apt-get update
 sudo apt-get install -y grafana
 
