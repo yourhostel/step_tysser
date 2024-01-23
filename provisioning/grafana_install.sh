52206c85d7a0c71b7aeed9a3ba4a9d320c314a02
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-01-23 06:38:03 +0200

    added source to grafana

--- provisioning/grafana_install.sh
+++ provisioning/grafana_install.sh
@@ -11,7 +11,39 @@ echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/a
 sudo apt-get update
 sudo apt-get install -y grafana
 
-# Обновление списка пакетов и установка Grafana
+# Створення конфігураційного файлу джерела даних для Prometheus
+sudo bash -c 'cat <<EOF > /etc/grafana/provisioning/datasources/prometheus-datasource.yaml
+apiVersion: 1
+
+datasources:
+- name: Prometheus
+  type: prometheus
+  access: proxy
+  orgId: 1
+  url: http://localhost:9090
+  isDefault: true
+  editable: true
+EOF'
+
+# Створення файлу конфігурації джерела даних MySQL Exporter
+sudo bash -c 'cat <<EOF > /etc/grafana/provisioning/datasources/mysql-exporter-datasource.yaml
+apiVersion: 1
+
+datasources:
+- name: MySQL Exporter
+  type: prometheus
+  access: proxy
+  orgId: 1
+  url: http://192.168.88.241:9104/metrics
+  isDefault: false
+  editable: true
+EOF'
+
+# Налаштування облікових даних адміністратора
+export GF_SECURITY_ADMIN_USER=admin
+export GF_SECURITY_ADMIN_PASSWORD=1234
+
+# Запуск Grafana
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
